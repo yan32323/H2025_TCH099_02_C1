9d20900fe5807cfac4535ba2b4a2ebca3bdeca9c
Jonathan Chartier <e2276400@cmaisonneuve.qc.ca>
2025-04-13 16:25:10 -0400

    Création de la route pour récupéré les recettes selon les filtres données

--- api/CreationRecettes.php
+++ api/CreationRecettes.php
@@ -160,5 +160,108 @@ $router->post('/CreationRecettes.php/recettes/supprimer/', function () {
     }
 });
 
+
+// Route pour récupéré toutes les recettes selon les 2 filtres données
+$router->post('/CreationRecettes.php/recuperer-recettes-filtrer', function () 
+{
+
+    require_once '../includes/conection.php';
+    header('Content-Type: application/json');
+
+    try {
+
+        //extraire les éléments de l'objet JSON
+        $data_json = file_get_contents("php://input");
+        $data = json_decode($data_json, true);
+
+        $identifiant = trim($data['identifiant']);
+        $motDePasse = trim($data['motDePasse']);
+        $filtreType = trim($data['filtreType']);
+        $filtreRestriction = trim($data['filtreRestriction']);
+
+        //Si le client exite dans la base de donnée, on récupère les recettes dans la base de données selon les filtres données
+        if(validateUserCredentials($identifiant, $motDePasse, $pdo)){
+
+            try{
+                // On prépare la requête SQL en fonction des filtres et on l'exécute
+                if(strcmp($filtreType, "tout") == 0){
+                    switch ($filtreRestriction) {
+                        case 'tout': 
+                            $requete = $pdo->prepare("SELECT r.id, r.nom, r.temps_de_cuisson, r.type, r.image FROM recettes AS r");
+                            $requete->execute();
+                        break;
+                        case 'sauvegarder':
+                            $requete = $pdo->prepare("SELECT r.id, r.nom, r.temps_de_cuisson, r.type, r.image FROM recettes AS r INNER JOIN recettes_sauvegardees AS r_s ON r.id = r_s.recette_id WHERE r_s.nom_utilisateur = :identifiant");
+                            $requete->execute([':identifiant' => $identifiant]);
+                            break;
+                        case 'miennes':
+                            $requete = $pdo->prepare("SELECT r.id, r.nom, r.temps_de_cuisson, r.type, r.image FROM recettes AS r WHERE r.createur_nom_utilisateur = :identifiant");
+                            $requete->execute([':identifiant' => $identifiant]);
+                            break;
+                        default:
+                            $requete = $pdo->prepare("SELECT r.id, r.nom, r.temps_de_cuisson, r.type, r.image FROM recettes AS r WHERE r.restriction = :restriction");
+                            $requete->execute([':restriction' => $filtreRestriction]);
+                            break;
+                    }
+                }else{
+                    switch ($filtreRestriction) {
+                        case 'tout': 
+                            $requete = $pdo->prepare("SELECT r.id, r.nom, r.temps_de_cuisson, r.type, r.image FROM recettes AS r WHERE r.type = :typeChoisi");
+                            $requete->execute([':typeChoisi' => $filtreType]);
+                            break;
+                        case 'sauvegarder':
+                            $requete = $pdo->prepare("SELECT r.id, r.nom, r.temps_de_cuisson, r.type, r.image FROM recettes AS r INNER JOIN recettes_sauvegardees AS r_s ON r.id = r_s.recette_id WHERE r_s.nom_utilisateur = :identifiant AND r.type = :typeChoisi");
+                            $requete->execute([':identifiant' => $identifiant, ':typeChoisi' => $filtreType]);
+                            break;
+                        case 'miennes':
+                            $requete = $pdo->prepare("SELECT r.id, r.nom, r.temps_de_cuisson, r.type, r.image FROM recettes AS r WHERE r.createur_nom_utilisateur = :identifiant AND r.type = :typeChoisi");
+                            $requete->execute([':identifiant' => $identifiant, ':typeChoisi' => $filtreType]);
+                            break;
+                        default:
+                            $requete = $pdo->prepare("SELECT r.id, r.nom, r.temps_de_cuisson, r.type, r.image FROM recettes AS r WHERE r.restriction = :restriction AND r.type = :typeChoisi");
+                            $requete->execute([':restriction' => $filtreRestriction, ':typeChoisi' => $filtreType]);
+                            break;
+                    }
+                }
+
+            //Erreur de la requête SQL
+            }catch(PDOException $e){
+                http_response_code(400); 
+                echo json_encode(['statut' => 'error', 'message' => 'Le type et/ou la restrictions fournis n\'est pas valide.']);
+                exit();
+            }
+
+            //On récupère les résultats de la requête et on les encode en JSON
+            $resultat = $requete->fetchAll(PDO::FETCH_ASSOC);
+            echo json_encode(['statut' => 'success', 'listeRecette' => $resultat]);
+            exit();
+
+        }else{
+            //Cas où l'authentification a échoué
+            http_response_code(401); 
+            echo json_encode(['statut' => 'error', 'message' => 'Authentification requise.']);
+            exit();
+        }
+
+    } catch (PDOException $e) {
+        http_response_code(401); 
+        echo json_encode(['statut' => 'error', 'message' => 'Erreur de connexion']);
+        exit();
+    }
+});
+
+function validateUserCredentials($identifiant, $motDePasse, $pdo) {
+
+    $requete = $pdo->prepare("SELECT mot_de_passe FROM clients WHERE nom_utilisateur = :identifiant");
+    $requete->execute([':identifiant' => $identifiant]);
+    $user = $requete->fetch(PDO::FETCH_ASSOC);
+
+    if ($user && password_verify($motDePasse, $user['mot_de_passe'])) {
+        return true; 
+    } else {
+        return false; 
+    }
+}
+
 // Acheminer la requête
 $router->dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
